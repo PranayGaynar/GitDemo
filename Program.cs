7a3bfebf2ccbc2999f8f194646d9d15602f1321b
Pranay <pranaygaynar123@gmail.com>
2024-10-30 12:33:52 +0530

    Added success

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGithub1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello,This Is Demo For Github!");
+            Console.ReadKey();
+
+
+        }
+    }
+}
